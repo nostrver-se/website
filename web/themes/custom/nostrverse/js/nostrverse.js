0411fd34c9449c8de1b873aea659b4a5800bcf69
Sebastix <info@sebastix.nl>
2024-10-22 16:09:12 +0200

    nostrverse theme

---
+++ web/themes/custom/nostrverse/js/nostrverse.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * nostrverse behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.nostrverse = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
